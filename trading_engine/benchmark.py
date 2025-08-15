8ae04108927a9e37bcb6502a97f3c5bf3583237e
Sage I. <168477625+sirfan2@users.noreply.github.com>
2025-08-15 16:36:56 -0500

    Update benchmark.py

--- trading_engine/benchmark.py
+++ trading_engine/benchmark.py
@@ -7,47 +7,6 @@ from datetime import datetime
 import uuid
 import os
 
-# Regular Order Book
-# DB_PATH = os.path.join(os.path.dirname(__file__), "trades.db")
-# logger = TradeLogger(db_name=DB_PATH)
-# engine = MatchingEngine(logger)
-
-# symbols = ["AAPL", "GOOG", "TSLA", "MSFT"]
-
-# def benchmark(num_orders=100_000):
-#     start = time.time()
-
-#     for _ in range(num_orders):
-#         symbol = random.choice(symbols)
-#         price = round(random.uniform(95, 105), 2)
-#         quantity = random.randint(1, 100)
-        
-#         buy_order = Order(
-#             id=str(uuid.uuid4()),
-#             symbol=symbol,
-#             side="BUY",
-#             price=price,
-#             quantity=quantity,
-#             timestamp=datetime.now()
-#         )
-#         sell_order = Order(
-#             id=str(uuid.uuid4()),
-#             symbol=symbol,
-#             side="SELL",
-#             price=price,
-#             quantity=quantity,
-#             timestamp=datetime.now()    
-#         )
-
-#         engine.add_order(symbol, buy_order)
-#         engine.add_order(symbol, sell_order)
-    
-#     elapsed = time.time() - start
-
-#     print(f"Processed {num_orders:,} orders in {elapsed:.4f} sec")
-#     print(f"Throughput {num_orders / elapsed:,.2f} orders/sec")
-#     print("Benchmark complete!")
-
 # Heap Order Book
 class InMemoryLogger():
     def __init__(self):
